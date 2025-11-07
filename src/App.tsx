a76f4a0a7f98d4f41328bc4c96071cc051da900c
Pedro Froeder <pedrofroeder22@gmail.com>
2025-11-07 16:41:01 -0300

    Melhorando

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { CartProvider } from "./contexts/CartContext";
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-      <CartProvider>
-        <Toaster position="top-right" />
-        <RouterProvider router={router} />
-      </CartProvider>
+      <Toaster position="top-right" />
+      <RouterProvider router={router} />
     </>
   );
 }
