0a69d47ecc025d3919156fe9e7948e12518b7623
Pedro Froeder <pedrofroeder22@gmail.com>
2025-11-09 11:53:21 -0300

    Adicionando ScrollRestoration

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Header } from "../Header";
 
 export function Layout() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <ScrollRestoration />
       <Header />
       <main>
         <Outlet />
